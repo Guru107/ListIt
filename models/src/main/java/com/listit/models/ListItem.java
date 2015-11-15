7400c639147581cc17e159a68badddc3a9a3072d
guru107 <guruak107@gmail.com>
2015-11-15 15:13:20 +0530

    ListItem setter and getter added

--- models/src/main/java/com/listit/models/ListItem.java
+++ models/src/main/java/com/listit/models/ListItem.java
@@ -1,4 +1,13 @@
 package com.listit.models;
 
 public class ListItem {
+    private String mContent;
+
+    public String getmContent() {
+        return mContent;
+    }
+
+    public void setmContent(String mContent) {
+        this.mContent = mContent;
+    }
 }
